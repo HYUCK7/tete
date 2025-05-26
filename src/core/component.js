ccc1f9aef2f2afee5320200dea0ca093c8f80d42
youjaehyuck <iqeq1219@4-lab.com>
2025-05-26 09:43:39 +0900

    `plate

--- src/core/component.js
+++ src/core/component.js
@@ -1,13 +1,29 @@
 export default class Component{
     $target;
     stateVal;
-    
-    constructor($target){
+    props;
+    constructor($target, props){
         this.$target = $target;
+        this.props = props;
         this.setup()
         this.setEvent()
+        this.render();
+      
     }
     setup(){}
     template(){return ''};
     setEvent(){}
+    effected(){}
+    render(){
+        this.$target.innerHTML = this.template();
+        this.effected();
+    }
+    addEvent(eventType, selector, callback){
+        const children = [...this.$target.querySelectorAll(selector)];
+        const isTarget = (target) => children.includes(target) || target.closest(selector);
+        this.$target.addEventListener(eventType, (event) => {
+            if(!isTarget(event.target)) return false;
+            callback(event);
+        })
+    }
 }
